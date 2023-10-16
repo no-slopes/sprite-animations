1e8942bc1016e36847f5c4c2fe5244ca2728f6b1
Gabo <indiegabodev@gmail.com>
2023-10-16 15:54:08 -0300

    Documenting enums

--- Runtime/Scripts/Enums.cs
+++ Runtime/Scripts/Enums.cs
@@ -1,12 +1,27 @@
 namespace SpriteAnimations
 {
+    /// <summary>
+    /// The update mode of the animator
+    /// </summary>
     public enum UpdateMode
     {
+        /// <summary>
+        /// Will evaluate frames every Update
+        /// </summary>
         Update,
+        /// <summary>
+        /// Will evaluate frames every FixedUpdate
+        /// </summary>
         FixedUpdate,
+        /// <summary>
+        /// Will evaluate frames every LateUpdate
+        /// </summary>
         LateUpdate,
     }
 
+    /// <summary>
+    /// A direction in the windrose (8-direction)
+    /// </summary>
     public enum WindroseDirection
     {
         North,
@@ -19,9 +34,18 @@ namespace SpriteAnimations
         NorthWest
     }
 
+    /// <summary>
+    /// The flip strategy for that animation
+    /// </summary>
     public enum FlipStrategy
     {
+        /// <summary>
+        /// The WindroseAnimator will flip East sprites to play West animations
+        /// </summary>
         Flip,
+        /// <summary>
+        /// There will be no flip at all.
+        /// </summary>
         NoFlip
     }
 }
