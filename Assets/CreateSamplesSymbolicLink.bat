def793800c501bf58a6d4439e45f647b3f6c603c
Gabo <indiegabodev@gmail.com>
2023-10-25 12:19:46 -0300

    Symlink created

--- Assets/CreateSamplesSymbolicLink.bat
+++ Assets/CreateSamplesSymbolicLink.bat
@@ -1,11 +1,12 @@
 @echo off
 
 SET root=%~dp0
-SET startPath=%root%Samples
+SET startPath=Samples
 SET destPath=_SpriteAnimations\Samples~
 
 echo %root%
 echo StartPath is: %startPath%
 echo DestPath is: %destPath%
 
-mklink /d %startPath% %destPath%
+mklink /D %startPath% %destPath%
+pause
